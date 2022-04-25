8d2b7343ac6cfbe4e95c87b831c50489ccb3195c
Spencer <Edwards>
2022-04-25 13:50:44 -0700

    added ant-table project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import dataSource from "./data.json"
+import { Table, Tag, Space } from "antd"
+const { Column, ColumnGroup } = Table;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table dataSource={dataSource.data}>
+      <ColumnGroup title="Name">
+        <Column title="First Name" dataIndex="firstName" key="firstName" />
+        <Column title="Last Name" dataIndex="lastName" key="lastName" />
+      </ColumnGroup>
+      <Column title="Age" dataIndex="age" key="age" />
+      <Column title="Address" dataIndex="address" key="address" />
+      <Column
+        title="Tags"
+        dataIndex="tags"
+        key="tags"
+        render={(tags: string[]) => (
+          <>
+            {tags.map((tag: string) => (
+              <Tag color="blue" key={tag}>
+                {tag}
+              </Tag>
+            ))}
+          </>
+        )}
+      />
+      <Column
+        title="Action"
+        key="action"
+        render={(_, record: {firstName: string, lastName: string}) => (
+          <Space size="middle">
+            <a>Invite {record.lastName}</a>
+            <a>Delete</a>
+          </Space>
+        )}
+      />
+   </Table>
     </div>
   );
 }
